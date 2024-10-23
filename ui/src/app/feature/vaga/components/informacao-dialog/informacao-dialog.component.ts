b0f4a585b09ed6a5733d9a715259c68a656956d6
lucianawessner <lucianawessner@hotmail.com>
2024-10-23 13:49:52 -0300

    Modificando Moldal

--- ui/src/app/feature/vaga/components/informacao-dialog/informacao-dialog.component.ts
+++ ui/src/app/feature/vaga/components/informacao-dialog/informacao-dialog.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-informacao-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatDialogModule
+  ],
   templateUrl: './informacao-dialog.component.html',
   styleUrl: './informacao-dialog.component.scss'
 })
 export class InformacaoDialogComponent {
+  jobTitle: string = "Desenvolvedor Frontend";
+  jobLocation: string = "São Paulo, SP";
+  jobSalary: string = "R$ 5.000,00";
+  jobDescription: string = "Desenvolvimento de aplicações web...";
+  jobRequirements: string = "Conhecimento em Angular, CSS, TypeScript...";
+  jobDeadline: string = "30/10/2024";
+  jobCreationDate: string = "01/10/2024";
+  jobClosingDate: string = "30/10/2024";
 
 }
