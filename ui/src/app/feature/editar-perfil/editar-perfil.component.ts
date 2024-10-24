047fe653c96fedb7f868de116a4b59913abf7b53
lucianawessner <lucianawessner@hotmail.com>
2024-10-24 14:56:50 -0300

    Adicionando - Editar-perfil

--- ui/src/app/feature/editar-perfil/editar-perfil.component.ts
+++ ui/src/app/feature/editar-perfil/editar-perfil.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal, ViewEncapsulation} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from  '@angular/material/input' ;
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {merge} from 'rxjs';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-editar-perfil',
@@ -9,11 +18,58 @@ import { MatMenuModule } from '@angular/material/menu';
   imports: [
     MatCardModule,
     MatFormFieldModule,
-    MatMenuModule
+    MatInputModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule
   ],
   templateUrl: './editar-perfil.component.html',
-  styleUrl: './editar-perfil.component.scss'
+  styleUrl: './editar-perfil.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  providers: [provideNativeDateAdapter()],
 })
 export class EditarPerfilComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    // Only highligh dates inside the month view.
+    if (view === 'month') {
+      const date = cellDate.getDate();
+
+      // Highlight the 1st and 20th day of each month.
+      return date === 1 || date === 20 ? 'example-custom-date-class' : '';
+    }
+
+    return '';
+  };
 
 }
