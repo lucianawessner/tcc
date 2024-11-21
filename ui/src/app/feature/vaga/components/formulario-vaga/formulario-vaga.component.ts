1fb02aaad461edf2ea1b88734e51314e3ec1ef55
lucianawessner <lucianawessner@hotmail.com>
2024-11-21 16:04:19 -0300

    Arrumando layout do formulario da vaga

--- ui/src/app/feature/vaga/components/formulario-vaga/formulario-vaga.component.ts
+++ ui/src/app/feature/vaga/components/formulario-vaga/formulario-vaga.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-formulario-vaga',
@@ -10,10 +14,27 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCardModule,
     MatFormFieldModule,
     MatDialogModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule
   ],
   templateUrl: './formulario-vaga.component.html',
-  styleUrl: './formulario-vaga.component.scss'
+  styleUrl: './formulario-vaga.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class FormularioVagaComponent {
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    // Only highligh dates inside the month view.
+    if (view === 'month') {
+      const date = cellDate.getDate();
 
+      // Highlight the 1st and 20th day of each month.
+      return date === 1 || date === 20 ? 'example-custom-date-class' : '';
+    }
+
+    return '';
+  };
 }
